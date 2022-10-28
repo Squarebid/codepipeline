066e94fa7f8471a067d82af55e9b5b619b6c15fc
kdavis <kevin.d@purple.com>
2022-10-28 16:37:58 +0000

    Initial Commit

---
+++ lib/pipeline-cdk-stack.ts
@@ -0,0 +1,24 @@
+import { Stack, StackProps, SecretValue } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+
+export class PipelineCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+    
+    
+    const sourceOutput = new codepipeline.Artifact();
+    
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'Squarebid',
+      repo: 'codepipeline',
+      oauthToken: SecretValue.secretsManager('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'),
+      output: sourceOutput,
+      branch: 'main', 
+      });
+      
+  }
+}
