8ed3fe3ea993935a7be271736b5fd450c6af0a11
kdavis <kevin.d@purple.com>
2022-10-28 19:08:45 +0000

    Implemented CI pipeline

--- lib/pipeline-cdk-stack.ts
+++ lib/pipeline-cdk-stack.ts
@@ -1,24 +1,66 @@
-import { Stack, StackProps, SecretValue } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
 import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
-import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-    
-    
+
+    const sourceRepo = new codecommit.Repository(this, "CICD_Workshop", {
+      repositoryName: "CICD_Workshop",
+      description: "Repository for my application code and infrastructure",
+    });
+
+    const pipeline = new codepipeline.Pipeline(this, "CICD_Pipeline", {
+      pipelineName: "CICD_Pipeline",
+      crossAccountKeys: false,
+    });
+
+    const codeQualityBuild = new codebuild.PipelineProject(
+      this,
+      "Code Quality",
+      {
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+          privileged: true,
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml')
+      }
+    );
+
     const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new codepipeline_actions.CodeCommitSourceAction({
+          actionName: "CodeCommit",
+          repository: sourceRepo,
+          output: sourceOutput,
+          branch: "main",
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: "Code-Quality-Testing",
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: "Unit-Test",
+          project: codeQualityBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
     
-    const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: 'GitHub_Source',
-      owner: 'Squarebid',
-      repo: 'codepipeline',
-      oauthToken: SecretValue.secretsManager('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'),
-      output: sourceOutput,
-      branch: 'main', 
-      });
-      
+
+    new CfnOutput(this, "CodeCommitRepositoryUrl", {
+      value: sourceRepo.repositoryCloneUrlHttp,
+    });
   }
 }
